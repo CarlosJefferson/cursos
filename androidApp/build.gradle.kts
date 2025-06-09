4e970a52f5a9db69abfe20094e586a014d19ca5a
Carlos Jefferson (JS) <carlos.jefferson@gmail.com>
2025-06-09 14:02:47 -0300

    Add Android app module

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application") version "8.4.1"
+    id("org.jetbrains.kotlin.android") version "2.1.20"
+}
+
+android {
+    namespace = "com.example.androidcalc"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.androidcalc"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        jvmToolchain(21)
+    }
+}
+
+dependencies {
+    implementation(project(":lib"))
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,29 @@
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.14/userguide/multi_project_builds.html in the Gradle documentation.
  */
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "calc-kmp"
 include("lib")
+include("androidApp")
